4b5bc382aa40f84900af39fc47b079be5dec40f4
David Persson <davidpersson@gmx.de>
2013-09-04 14:15:13 +0200

    Make admin layout notify ready.

--- views/layouts/admin.html.php
+++ views/layouts/admin.html.php
@@ -1,9 +1,13 @@
 <?php
 
 use lithium\core\Environment;
+use li3_flash_message\extensions\storage\FlashMessage;
 
 $site = Environment::get('site');
 $modules = Environment::get('modules');
+
+$flash = FlashMessage::read();
+FlashMessage::clear();
 
 ?>
 <!doctype html>
@@ -20,30 +24,40 @@ $modules = Environment::get('modules');
 		<?php echo $this->html->script(array(
 			'/assets/core/js/underscore',
 			'/assets/core/js/jquery',
-			'/assets/core/js/require'
+			'/assets/core/js/require',
+			'/assets/core/js/base'
 		)) ?>
 		<?php echo $this->scripts() ?>
+
 	</head>
-	<body>
-	<div id="container">
-		<header>
-			<h1>
-				<?= $site['title'] ?> –
-				<?= $this->html->link('Administration', array('controller' => 'pages', 'action' => 'home', 'library' => 'cms_core')) ?>
-			</h1>
-			<nav>
-				<?php foreach ($modules as $module): ?>
-					<?= $this->html->link($module['title'], array(
-						'controller' => $module['name'], 'action' => 'index', 'library' => $module['library']
-					)) ?>
-				<?php endforeach ?>
-				<?= $this->html->link('View Site', '/', array('target' => 'new')) ?>
-				<?= $this->html->link('Dashboard', array('controller' => 'pages', 'action' => 'home', 'library' => 'cms_core')) ?>
-			</nav>
-		</header>
-		<div id="content">
-			<?php echo $this->content(); ?>
+	<body class="layout-admin">
+		<div
+			id="messages"
+			<?php if ($flash): ?>
+				data-flash-message="<?= $flash['message'] ?>"
+				data-flash-level="<?= isset($flash['attr']['level']) ? $flash['attr']['level'] : 'neutral' ?>"
+			<?php endif ?>
+		>
 		</div>
-	</div>
+		<div id="container">
+			<header>
+				<h1>
+					<?= $site['title'] ?> –
+					<?= $this->html->link('Administration', array('controller' => 'pages', 'action' => 'home', 'library' => 'cms_core')) ?>
+				</h1>
+				<nav>
+					<?php foreach ($modules as $module): ?>
+						<?= $this->html->link($module['title'], array(
+							'controller' => $module['name'], 'action' => 'index', 'library' => $module['library']
+						)) ?>
+					<?php endforeach ?>
+					<?= $this->html->link('View Site', '/', array('target' => 'new')) ?>
+					<?= $this->html->link('Dashboard', array('controller' => 'pages', 'action' => 'home', 'library' => 'cms_core')) ?>
+				</nav>
+			</header>
+			<div id="content">
+				<?php echo $this->content(); ?>
+			</div>
+		</div>
 	</body>
 </html>

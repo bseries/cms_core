71ed3abf956e1aee06af11d5f3744c4d63728b88
David Persson <davidpersson@gmx.de>
2013-09-05 17:04:53 +0200

    Adding tokens model.

---
+++ models/Tokens.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use lithium\util\String;
+
+class Tokens extends \lithium\data\Model {
+
+	// Generates a random token either short (8 chars) or long (16 chars) and
+	// returns it. Default expiration is one year.
+	public static function generate($short = false) {
+		$token = substr(md5(String::random(32)), 0, $short ? 8 : 16);
+		$expires = date('Y-m-d H:i:s', time() + YEAR);
+
+		static::create(compact('token', 'expires'));
+		return static::save() ? $token : false;
+	}
+
+	// Checks if a given token is valid and not yet expired.
+	public static function check($token) {
+		return (boolean) static::find('count', array(
+			'conditions' => array(
+				'token' => $token,
+				'expires' => '>=' . date('Y-m-d H:i:s')
+			)
+		));
+	}
+
+	// Deletes a given token if it exists.
+	public static function void($token) {
+		$item = static::find('first', array(
+			'conditions' => compact('token'),
+			'fields' => array('id')
+		));
+		if (!$item) {
+			return false;
+		}
+		return $item->delete();
+	}
+
+	// Runs garbage collection on the collection, deleting any expired items.
+	public static function gc() {
+		return static::remove(array(
+			'expires <' => date('Y-m-d H:i:s')
+		));
+	}
+}
+
+?>

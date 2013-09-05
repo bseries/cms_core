bda726dbb937a4bd598415440a140d9c17af935d
David Persson <davidpersson@gmx.de>
2013-09-05 13:36:03 +0200

    Prepare cookie session config; remove http auth.

--- config/bootstrap/session.php
+++ config/bootstrap/session.php
@@ -1,36 +1,20 @@
 <?php
 
 use lithium\storage\Session;
-use lithium\security\Auth;
 use lithium\action\Dispatcher;
-use lithium\action\Response;
 use lithium\core\Environment;
 
-$name = basename(LITHIUM_APP_PATH);
-Session::config(array(
-	// 'cookie' => array('adapter' => 'Cookie', 'name' => $name),
-	'default' => array('adapter' => 'Php', 'session.name' => $name)
-));
-
 Dispatcher::applyFilter('_call', function($self, $params, $chain) {
-	// late...
-	Auth::config(array(
-		'http' => array(
-			'adapter' => 'Http',
-			'realm' => 'Application Administration',
-			'method' => 'digest',
-			'users' => Environment::get('users')
-		)
+	// late... plugin comes before app
+	Session::config(array(
+		'default' => array('adapter' => 'Php', 'session.name' => Environment::get('project.name'))
+		// 'default' => array(
+		// 	'adapter' => 'Cookie',
+		// 	'strategies' => array(
+		// 		'Hmac' => array('secret' => Environment::get('security.cookieSecret'))
+		// 	)
+		// )
 	));
-
-	$url = $params['request']->url;
-
-	$granted = strpos($url, 'admin') !== 0;
-	$granted = $granted || Auth::check('http', $params['request']);
-
-	if (!$granted) {
-		return new Response(array('status' => 401, 'body' => 'Access denied.'));
-	}
 	return $chain->next($self, $params, $chain);
 });
 

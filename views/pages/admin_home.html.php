a0cef87bb2d07e6fb6f46c609fd839ec67cae432
David Persson <davidpersson@gmx.de>
2013-09-06 17:55:17 +0200

    Feature switches in dashboard.

--- views/pages/admin_home.html.php
+++ views/pages/admin_home.html.php
@@ -4,15 +4,19 @@ use lithium\core\Environment;
 
 $services = Environment::get('service');
 $site = Environment::get('site');
+$features = Environment::get('features');
 
 ?>
 <article class="view-<?= $this->_config['controller'] . '-' . $this->_config['template'] ?>">
 	<h1><?= $this->title('Dashboard') ?></h1>
 
 	<h2>Site</h2>
-<pre>
-<?php echo json_encode($site) ?>
-</pre>
+	<dl>
+	<?php foreach ($site as $name => $value): ?>
+		<dt><?= $name ?></dt>
+		<dd><?= $value ?></dd>
+	<?php endforeach ?>
+	</dl>
 
 	<h2>Registered Services</h2>
 	<?php foreach ($services as $name => $service): ?>
@@ -21,4 +25,13 @@ $site = Environment::get('site');
 <?php echo json_encode($service) ?>
 </pre>
 	<?php endforeach ?>
+
+	<h2><?= $t('Features') ?></h2>
+	<dl>
+	<?php foreach ($features as $name => $value): ?>
+		<dt><?= $name ?></dt>
+		<dd><?= $value ? $t('enabled') : $t('disabled')	 ?></dd>
+	<?php endforeach ?>
+	</dl>
 </article>
+

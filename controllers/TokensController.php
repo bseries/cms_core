a5e12e4da792568c845bee763de38599d43e97a8
David Persson <davidpersson@gmx.de>
2013-09-06 13:13:52 +0200

    Adding tokens controller.

---
+++ controllers/TokensController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace cms_core\controllers;
+
+use cms_core\models\Tokens;
+use li3_flash_message\extensions\storage\FlashMessage;
+
+class TokensController extends \lithium\action\Controller {
+
+	public function admin_index() {
+		$data = Tokens::find('all');
+		return compact('data');
+	}
+
+	public function admin_generate() {
+		Tokens::generate();
+
+		FlashMessage::write('Token generated.');
+		return $this->redirect(array('action' => 'index', 'library' => 'cms_core'));
+	}
+
+	public function admin_void() {
+		Tokens::void($token = $this->request->token);
+
+		FlashMessage::write("Voided token `{$token}`.");
+		return $this->redirect(array('action' => 'index', 'library' => 'cms_core'));
+	}
+}
+
+?>

b246fd7cd483ee255df3ec7853c70d54dc325107
David Persson <davidpersson@gmx.de>
2013-09-06 11:01:26 +0200

    Base requirejs config.

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,4 +1,55 @@
-require(['jquery', 'notify', '!domready'], function($) {
+
+var App = {
+  'debugCompat': false
+};
+
+App.env = {
+};
+
+requirejs.config({
+  urlArgs: "v=REV",
+  baseUrl: '//' + window.location.hostname + '/assets',
+  waitSeconds: 15,
+  paths: {
+    'jquery': 'core/js/jquery',
+    'underscore': 'core/js/underscore',
+    'notify': 'core/js/notify',
+    'domready': 'core/js/domready'
+  },
+  shim: {
+    'globalize': {
+      deps: ['jquery'],
+      exports: 'Globalize'
+    },
+    'globalize.en.messages': {
+      deps: ['globalize']
+    },
+    'globalize.de': {
+      deps: ['globalize']
+    },
+    'globalize.de.messages': {
+      deps: ['globalize']
+    },
+    'modernizr': {
+      exports: 'Modernizr',
+      deps: ['domready!']
+    },
+    'versioncompare': {
+      exports: 'versionCompare'
+    },
+    'html5placeholder': {},
+    'notify': {
+      deps: ['jquery']
+    },
+    'underscore': {
+      exports: '_'
+    },
+    'jquery': {
+      exports: '$'
+    }
+  }
+});
+require(['jquery', 'notify', 'domready!'], function($) {
   // Bridge between PHP flash messaging and JS notify.
   var flashMessage = $('#messages').data('flash-message');
   var flashLevel = $('#messages').data('flash-class');

68d9c566c29cea3e747936e24c15b32ba7e0993d
David Persson <davidpersson@gmx.de>
2013-09-06 16:49:37 +0200

    Remove late init workaround for session.

--- config/bootstrap/session.php
+++ config/bootstrap/session.php
@@ -1,21 +1,16 @@
 <?php
 
 use lithium\storage\Session;
-use lithium\action\Dispatcher;
 use lithium\core\Environment;
 
-Dispatcher::applyFilter('_call', function($self, $params, $chain) {
-	// late... plugin comes before app
-	Session::config(array(
-		'default' => array('adapter' => 'Php', 'session.name' => Environment::get('project.name'))
-		// 'default' => array(
-		// 	'adapter' => 'Cookie',
-		// 	'strategies' => array(
-		// 		'Hmac' => array('secret' => Environment::get('security.cookieSecret'))
-		// 	)
-		// )
-	));
-	return $chain->next($self, $params, $chain);
-});
+Session::config(array(
+	'default' => array('adapter' => 'Php', 'session.name' => Environment::get('project.name'))
+	// 'default' => array(
+	// 	'adapter' => 'Cookie',
+	// 	'strategies' => array(
+	// 		'Hmac' => array('secret' => Environment::get('security.cookieSecret'))
+	// 	)
+	// )
+));
 
 ?>

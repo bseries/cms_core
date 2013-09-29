44dee0e7248268b379b8ae6747d2b3ece8434aab
David Persson <davidpersson@gmx.de>
2013-09-29 14:31:10 +0200

    Enable analytics if we have settings.

--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -45,7 +45,7 @@ FlashMessage::clear();
 		<![endif]-->
 		<?php echo $this->styles() ?>
 		<?php echo $this->scripts() ?>
-		<?php if (Environment::is('production')): ?>
+		<?php if (isset($service['googleAnalytics'])): ?>
 			<?=$this->view()->render(['element' => 'ga'], $service['googleAnalytics'], [
 				'library' => 'cms_core'
 			]) ?>

51ce017b2ad97c983285d7076caf9bd7e4c2fbb2
David Persson <davidpersson@gmx.de>
2013-10-25 13:01:42 +0200

    None available for tokens.

--- views/tokens/admin_index.html.php
+++ views/tokens/admin_index.html.php
@@ -13,26 +13,29 @@ $dateFormatter = new IntlDateFormatter(
 	<nav class="actions">
 		<?= $this->html->link($t('generate token'), ['action' => 'generate', 'library' => 'cms_core'], ['class' => 'button']) ?>
 	</nav>
-
-	<table>
-		<thead>
-			<tr>
-				<td><?= $t('Token') ?>
-				<td><?= $t('Expires') ?>
-				<td>
-		</thead>
-		<tbody>
-			<?php foreach ($data as $item): ?>
-			<tr>
-				<td><code><?= $item->token ?></code>
-				<td>
-					<?php $date = DateTime::createFromFormat('Y-m-d H:i:s', $item->expires) ?>
-					<time datetime="<?= $date->format(DateTime::W3C) ?>"><?= $dateFormatter->format($date) ?></time>
-				<td>
-					<nav class="actions">
-						<?= $this->html->link($t('void'), ['token' => $item->token, 'action' => 'void', 'library' => 'cms_core'], ['class' => 'button']) ?>
-					</nav>
-			<?php endforeach ?>
-		</tbody>
-	</table>
+	<?php if ($data->count()): ?>
+		<table>
+			<thead>
+				<tr>
+					<td><?= $t('Token') ?>
+					<td><?= $t('Expires') ?>
+					<td>
+			</thead>
+			<tbody>
+				<?php foreach ($data as $item): ?>
+				<tr>
+					<td><code><?= $item->token ?></code>
+					<td>
+						<?php $date = DateTime::createFromFormat('Y-m-d H:i:s', $item->expires) ?>
+						<time datetime="<?= $date->format(DateTime::W3C) ?>"><?= $dateFormatter->format($date) ?></time>
+					<td>
+						<nav class="actions">
+							<?= $this->html->link($t('void'), ['token' => $item->token, 'action' => 'void', 'library' => 'cms_core'], ['class' => 'button']) ?>
+						</nav>
+				<?php endforeach ?>
+			</tbody>
+		</table>
+	<?php else: ?>
+		<div class="none-available"><?= $t('No tokens available, yet.') ?></div>
+	<?php endif ?>
 </article>

1a29c433a15ee38d54ca0b70cd3a5735cda1d17b
David Persson <davidpersson@gmx.de>
2014-01-09 16:18:28 +0100

    Updating AD contact details.

--- views/elements/contact_atelier_disko.html.php
+++ views/elements/contact_atelier_disko.html.php
@@ -2,12 +2,12 @@
 	<p>
 		<span itemprop="name">Atelier Disko</span><br/>
 		<div itemprop="address" itemtype="http://data-vocabulary.org/Address" itemscope>
-			<span itemprop="street-address">Rehhoffstraße 13</span><br/>
+			<span itemprop="street-address">Budapester Straße 49</span><br/>
 			<span itemprop="postal-code">20459</span> <span itemprop="locality">Hamburg</span><br/>
 		</div>
 	</p>
 	<p>
-		<label>Telefon:</label> <span itemprop="tel">+49 (0)40 32 96 89 72</span><br/>
+		<label>Telefon:</label> <span itemprop="tel">+49 (0) 40 355 618 96</span><br/>
 		<label>E–Mail:</label> <a href="mailto:hello@atelierdisko.de">hello@atelierdisko.de</a><br/>
 		<label>Website:</label> <a itemprop="url" href="http://atelierdisko.de" target="new">atelierdisko.de</a><br/>
 	</p>

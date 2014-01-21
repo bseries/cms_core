8b4023539df37065bbfe7ed658bc6916c17be429
David Persson <davidpersson@gmx.de>
2014-01-21 17:51:24 +0100

    Adding base controller.

---
+++ controllers/BaseController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Bureau
+ *
+ * Copyright (c) 2013-2014 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+namespace cms_core\controllers;
+
+class BaseController extends \lithium\action\Controller {}
+
+?>

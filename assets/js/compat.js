d246eecee44f04f447b0f15da4ea1a0bd6fcec7f
David Persson <davidpersson@gmx.de>
2014-01-25 12:28:54 +0100

    Reword browser compat.

--- assets/js/compat.js
+++ assets/js/compat.js
@@ -16,9 +16,27 @@ function($, _, versionCompare, Modernizr) {
   // We split browsers into groups of HTML5 (modern) and HTML4 (legacy).
   // Assignment to one group or another is done by using feature detection.
   //
+  // "The Mustard Test"
+  //
   // This approach replaces the user agent sniffing technique deployed
   // before as that is considered to be bad practice and support for
   // sniffing has been removed from jQuery >= 1.9 already.
+  //
+  // 1. legacy browsers (never work, force upgrade)
+  // 2. legacy browser, explictly excluded and supported (degraded expirience)
+  //
+  // If you want to support a legacy browser (IE8) they must be explictely excluded
+  // from the redirect and workarounds added elsewhere. If your contract says to
+  // support that one browser version you may actually want to fall back to browser
+  // sniffing to fullfill the contract to the letter.
+  //
+  // 3. modern browsers, tested and deemed supported (full expierience)
+  // 4. modern browsers, untested (probably work, expierence unknown)
+  //
+  // When a browser is deemed modern, it doesn't mean it is supported. You must
+  // explicitly test the browser and add workarounds where needed.  Don't assume
+  // that all modern browsers will just work and use the list below to present
+  // compatibility in a contract.
   //
   // Modern browsers are:
   //
@@ -43,6 +61,7 @@ function($, _, versionCompare, Modernizr) {
     modern = false;
   }
 
+  // Users of legacy browsers will by default be redirected to a browser upgrade page.
   url = '/browser';
   if (!modern && window.location != url) {
     all.browser = function() {

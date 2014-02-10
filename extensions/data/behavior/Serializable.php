406763d8fe57fe4267a15f7e39021a6a3ac4d9db
David Persson <davidpersson@gmx.de>
2014-02-10 11:32:52 +0100

    Updating serializable to use mostly static methods.

--- extensions/data/behavior/Serializable.php
+++ extensions/data/behavior/Serializable.php
@@ -17,31 +17,18 @@ use lithium\util\Set;
 
 class Serializable extends \li3_behaviors\data\model\Behavior {
 
-	protected $_methods = [];
+	protected function _config($config, $defaults) {
+		$config += ['fields' => []];
 
-	protected $_defaults = [
-		'fields' => []
-	];
-
-	protected function _init() {
-		parent::_init();
-
-		$this->_config += $this->_defaults;
-
-		$behavior = $this;
-		$model = $this->_model;
-
-		if (isset($this->_config['fields'])) {
-			$config['fields'] = Set::normalize($this->_config['fields']);
-
-			foreach ($config['fields'] as $field => &$pass) {
-				if (!$pass) {
-					$pass = 'json';
-				}
+		foreach (Set::normalize($config['fields']) as $field => &$pass) {
+			if (!$pass) {
+				$pass = 'json';
 			}
 		}
 		$this->_config = $config;
+	}
 
+	protected function _filters($model, $behavior, $config) {
 		$model::applyFilter('save', function($self, $params, $chain) use ($behavior, $config) {
 			$model = $self;
 
@@ -49,46 +36,34 @@ class Serializable extends \li3_behaviors\data\model\Behavior {
 				if (!isset($params['data'][$field])) {
 					continue;
 				}
-				$params['data'][$field] = $behavior->invokeMethod('_normalize', [
+				$params['data'][$field] = $behavior::invokeMethod('_normalize', [
 					$params['data'][$field], $type
 				]);
-				$params['data'][$field] = $behavior->invokeMethod('_serialize', [
+				$params['data'][$field] = $behavior::invokeMethod('_serialize', [
 					$params['data'][$field], $type
 				]);
 			}
 			return $chain->next($self, $params, $chain);
 		});
 
+		$methods = [];
 		foreach ($this->_config['fields'] as $field => $pass) {
-			$this->_methods[$field] = function($entity, array $options = []) use ($behavior, $field, $pass) {
+			$methods[$field] = function($entity, array $options = []) use ($behavior, $field, $pass) {
 				$options += ['serialized' => true];
 
 				$result = $entity->{$field};
-				$result = $behavior->invokeMethod('_normalize', [$result, $pass]);
+				$result = $behavior::invokeMethod('_normalize', [$result, $pass]);
 
 				if (!$options['serialized']) {
 					return $result;
 				}
-				return $behavior->invokeMethod('_serialize', [$result, $pass]);
+				return $behavior::invokeMethod('_serialize', [$result, $pass]);
 			};
 		}
+		$model::instanceMethods($methods);
 	}
 
-	public function respondsTo($method, $internal = false) {
-		if (isset($this->_methods[$method])) {
-			return true;
-		}
-		return parent::respondsTo($method, $internal);
-	}
-
-	public function __call($method, $params) {
-		if (isset($this->_methods[$method])) {
-			return $this->_methods[$method]($params[0]);
-		}
-		parent::__call($method, $params);
-	}
-
-	protected function _normalize($value, $pass) {
+	protected static function _normalize($value, $pass) {
 		$normalize = function($values) {
 			$values = array_filter(array_map('trim', $values));
 			sort($values);
@@ -101,14 +76,14 @@ class Serializable extends \li3_behaviors\data\model\Behavior {
 		} elseif ($value === null) {
 			return [];
 		} elseif (is_string($value)) {
-			return $normalize($this->_unserialize($value, $pass));
+			return $normalize(static::_unserialize($value, $pass));
 		} elseif (is_array($value)) {
 			return $normalize($value);
 		}
 		throw new Exception('Field value is in unsupported format.');
 	}
 
-	protected function _serialize($value, $type) {
+	protected static function _serialize($value, $type) {
 		switch ($type) {
 			case 'php':
 				return serialize($value);
@@ -119,7 +94,7 @@ class Serializable extends \li3_behaviors\data\model\Behavior {
 		}
 	}
 
-	protected function _unserialize($value, $type) {
+	protected static function _unserialize($value, $type) {
 		switch ($type) {
 			case 'php':
 				return unserialize($value);

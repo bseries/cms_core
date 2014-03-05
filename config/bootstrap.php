7c6cb5eea9976f643a5115f908f029346cdada06
David M. Persson <davidpersson@gmx.de>
2014-03-05 15:06:55 +0100

    Register useBilling feature refactor config.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -11,13 +11,8 @@
  */
 
 use lithium\core\Libraries;
-use lithium\g11n\Message;
-use cms_core\extensions\cms\Panes;
 use cms_core\extensions\cms\Features;
-use cms_core\extensions\cms\Settings;
 use cms_core\models\Assets;
-
-extract(Message::aliases());
 
 Libraries::add('li3_behaviors', [
 	'path' => dirname(__DIR__) . '/libraries/li3_behaviors'
@@ -62,66 +57,8 @@ require dirname(__DIR__) . '/libraries/guzzle/vendor/autoload.php';
 //	'path' => dirname(__DIR__) . '/libraries/guzzle/src',
 // ]);
 
-require __DIR__ . '/bootstrap/errors.php';
-require __DIR__ . '/bootstrap/action.php';
-
-if (PHP_SAPI !== 'cli') {
-	require __DIR__ . '/bootstrap/cache.php';
-}
-require __DIR__ . '/bootstrap/session.php';
-require __DIR__ . '/bootstrap/g11n.php';
-require __DIR__ . '/bootstrap/media.php';
-
-require __DIR__ . '/bootstrap/settings.php';
-
-if (PHP_SAPI === 'cli') {
-	require __DIR__ . '/bootstrap/console.php';
-}
-
-require __DIR__ . '/bootstrap/auth.php';
-
-Panes::register('cms_core', 'users', [
-	'title' => $t('Users'),
-	'group' =>  Panes::GROUP_ACCESS,
-	'url' => $base = ['controller' => 'users', 'action' => 'index', 'library' => 'cms_core', 'admin' => true],
-	'actions' => [
-		$t('List Users') => ['action' => 'index'] + $base,
-		$t('New User') => ['action' => 'add'] + $base
-	]
-]);
-Panes::register('cms_core', 'tokens', [
-	'title' => $t('Tokens'),
-	'group' => Panes::GROUP_ACCESS,
-	'url' => $base = ['controller' => 'tokens', 'action' => 'index', 'library' => 'cms_core', 'admin' => true],
-	'actions' => [
-		$t('List Tokens') => ['action' => 'index'] + $base,
-		$t('Generate Token') => ['action' => 'generate'] + $base
-	]
-]);
-Panes::register('cms_core', 'settings', [
-	'title' => $t('Settings & Features'),
-	'group' => Panes::GROUP_MANAGE,
-	'url' => ['controller' => 'settings', 'action' => 'index', 'library' => 'cms_core', 'admin' => true]
-]);
-Panes::register('cms_core', 'google_analytics', [
-	'title' => $t('Google Analytics'),
-	'group' => Panes::GROUP_MANAGE,
-	'url' => function() {
-		return 'https://www.google.com/analytics/web/#report/visitors-overview/' . Settings::read('googleAnalytics.default.propertyId');
-	}
-]);
-Panes::register('cms_core', 'styleguide', [
-	'title' => $t('Styleguide'),
-	'group' => Panes::GROUP_MANAGE,
-	'url' => ['controller' => 'pages', 'action' => 'styleguide', 'admin' => true, 'library' => 'cms_core'],
-]);
-Panes::register('cms_core', 'support', [
-	'title' => $t('Support'),
-	'group' => Panes::GROUP_MANAGE,
-	'url' => ['controller' => 'pages', 'action' => 'support', 'library' => 'cms_core', 'admin' => true]
-]);
-
 Features::register('cms_core', 'useNewGoogleAnalyticsTrackingCode', true);
+Features::register('cms_core', 'useBilling', false);
 
 // Register "empty" schemes, base must be set
 // through app. Cannot provide sane defaults here.
---
+++ config/panes.php
@@ -0,0 +1,50 @@
+<?php
+
+use cms_core\extensions\cms\Panes;
+use lithium\g11n\Message;
+use cms_core\extensions\cms\Settings;
+
+extract(Message::aliases());
+
+Panes::register('cms_core', 'users', [
+	'title' => $t('Users'),
+	'group' =>  Panes::GROUP_ACCESS,
+	'url' => $base = ['controller' => 'users', 'action' => 'index', 'library' => 'cms_core', 'admin' => true],
+	'actions' => [
+		$t('List Users') => ['action' => 'index'] + $base,
+		$t('New User') => ['action' => 'add'] + $base
+	]
+]);
+Panes::register('cms_core', 'tokens', [
+	'title' => $t('Tokens'),
+	'group' => Panes::GROUP_ACCESS,
+	'url' => $base = ['controller' => 'tokens', 'action' => 'index', 'library' => 'cms_core', 'admin' => true],
+	'actions' => [
+		$t('List Tokens') => ['action' => 'index'] + $base,
+		$t('Generate Token') => ['action' => 'generate'] + $base
+	]
+]);
+Panes::register('cms_core', 'settings', [
+	'title' => $t('Settings & Features'),
+	'group' => Panes::GROUP_MANAGE,
+	'url' => ['controller' => 'settings', 'action' => 'index', 'library' => 'cms_core', 'admin' => true]
+]);
+Panes::register('cms_core', 'google_analytics', [
+	'title' => $t('Google Analytics'),
+	'group' => Panes::GROUP_MANAGE,
+	'url' => function() {
+		return 'https://www.google.com/analytics/web/#report/visitors-overview/' . Settings::read('googleAnalytics.default.propertyId');
+	}
+]);
+Panes::register('cms_core', 'styleguide', [
+	'title' => $t('Styleguide'),
+	'group' => Panes::GROUP_MANAGE,
+	'url' => ['controller' => 'pages', 'action' => 'styleguide', 'admin' => true, 'library' => 'cms_core'],
+]);
+Panes::register('cms_core', 'support', [
+	'title' => $t('Support'),
+	'group' => Panes::GROUP_MANAGE,
+	'url' => ['controller' => 'pages', 'action' => 'support', 'library' => 'cms_core', 'admin' => true]
+]);
+
+?>

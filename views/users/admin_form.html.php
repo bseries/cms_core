eb6800e10e9e9f228cda3d41d15a90e5fcb45b32
David M. Persson <davidpersson@gmx.de>
2014-03-05 15:07:27 +0100

    Implement adv fileds for users.

--- views/users/admin_form.html.php
+++ views/users/admin_form.html.php
@@ -1,5 +1,7 @@
 <?php
 
+use cms_core\extensions\cms\Features;
+
 $title = [
 	'action' => ucfirst($this->_request->action === 'add' ? $t('creating') : $t('editing')),
 	'title' => $item->name ?: $t('untitled'),
@@ -30,7 +32,7 @@ require(['jquery', 'modal', 'domready!'], function($, modal) {
 </script>
 <?php $this->scripts(ob_get_clean()) ?>
 
-<article class="view-<?= $this->_config['controller'] . '-' . $this->_config['template'] ?>">
+<article class="view-<?= $this->_config['controller'] . '-' . $this->_config['template'] ?> section-spacing">
 	<h1 class="alpha">
 		<span class="action"><?= $title['action'] ?></span>
 		<span class="title"><?= $title['title'] ?></span>
@@ -43,18 +45,41 @@ require(['jquery', 'modal', 'domready!'], function($, modal) {
 	</nav>
 
 	<?=$this->form->create($item) ?>
-		<?= $this->form->field('id', ['type' => 'hidden']) ?>
-		<?= $this->form->field('name', ['type' => 'text', 'label' => $t('Name')]) ?>
-		<?= $this->form->field('email', ['type' => 'email', 'label' => $t('E–mail')]) ?>
-		<?= $this->form->field('role', [
-			'type' => 'select',
-			'label' => $t('Role'),
-			'list' => $roles
-		]) ?>
-		<?= $this->form->field('is_active', ['type' => 'checkbox', 'label' => $t('Aktiv?')]) ?>
+		<section>
+			<?= $this->form->field('id', ['type' => 'hidden']) ?>
+			<?= $this->form->field('name', ['type' => 'text', 'label' => $t('Name'), 'class' => 'title']) ?>
+			<?= $this->form->field('email', ['type' => 'email', 'label' => $t('E–mail')]) ?>
+			<?= $this->form->field('role', [
+				'type' => 'select',
+				'label' => $t('Role'),
+				'list' => $roles
+			]) ?>
+			<?= $this->form->field('is_active', ['type' => 'checkbox', 'label' => $t('Aktiv?')]) ?>
 
-		<?=$this->form->field('password', ['type' => 'password', 'label' => 'Neues Passwort']) ?>
-		<?=$this->form->field('password_repeat', ['type' => 'password', 'label' => 'Neues Passwort (wiederholen)']) ?>
+			<?= $this->form->field('timezone', [
+				'type' => 'select',
+				'label' => $t('Timezone'),
+				'list' => $timezones
+			]) ?>
+		</section>
+		<?php if (Features::enabled('useBilling')): ?>
+			<section>
+				<?= $this->form->field('billing_currency', [
+					'type' => 'select',
+					'label' => $t('Billing Currency'),
+					'list' => $currencies
+				]) ?>
+				<?= $this->form->field('billing_vat_reg_no', [
+					'type' => 'text',
+					'autocomplete' => 'off',
+					'label' => $t('Billing VAT Reg. No.')
+				]) ?>
+			</section>
+		<?php endif ?>
+		<section>
+			<?=$this->form->field('password', ['type' => 'password', 'label' => 'Neues Passwort', 'autocomplete' => 'off']) ?>
+			<?=$this->form->field('password_repeat', ['type' => 'password', 'label' => 'Neues Passwort (wiederholen)', 'autocomplete' => 'off']) ?>
+		</section>
 		<?= $this->form->button($t('save'), ['type' => 'submit', 'class' => 'large']) ?>
 	<?=$this->form->end() ?>
 </article>

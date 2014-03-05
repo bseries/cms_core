e1697c9917aa42e6427377840f77258469658ca0
David M. Persson <davidpersson@gmx.de>
2014-03-05 12:12:23 +0100

    Use project version for etags.

--- config/bootstrap/cache.php
+++ config/bootstrap/cache.php
@@ -58,7 +58,8 @@ Dispatcher::applyFilter('run', function($self, $params, $chain) {
 
 	$hash = 'W/' . md5(serialize([
 		$response->body,
-		$response->headers
+		$response->headers,
+		PROJECT_VERSION
 	]));
 	$condition = trim($request->get('http:if_none_match'), '"');
 

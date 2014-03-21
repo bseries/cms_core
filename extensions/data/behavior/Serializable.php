efbf159f68e0e72efe30b9202cc841a62b5276a3
David M. Persson <davidpersson@gmx.de>
2014-03-21 17:48:43 +0100

    Update li3_behaviors.

--- extensions/data/behavior/Serializable.php
+++ extensions/data/behavior/Serializable.php
@@ -21,7 +21,7 @@ class Serializable extends \li3_behaviors\data\model\Behavior {
 		'fields' => [],
 	];
 
-	protected static function _config($config, $defaults) {
+	protected static function _config($model, $behavior, $config, $defaults) {
 		$config += $defaults;
 		$config['fields'] = Set::normalize($config['fields']);
 

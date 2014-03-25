524049680e4d6799856ef7ffefa8b71a3adb9f93
David M. Persson <davidpersson@gmx.de>
2014-03-25 17:52:46 +0100

    Improve and fix features and settings.

--- extensions/cms/Features.php
+++ extensions/cms/Features.php
@@ -13,26 +13,53 @@
 namespace cms_core\extensions\cms;
 
 use lithium\core\Environment;
+use lithium\util\Set;
 
+// Non hirachical storage.
 class Features extends \lithium\core\StaticObject {
+
+	protected static $_data = [];
 
 	protected static $_sources = [];
 
 	public static function register($library, $name, $default) {
 		static::$_sources[$name] = $library;
-		Environment::set(true, ['features' => [$name => $default]]);
+		static::$_data = Set::merge(static::$_data, [$name => $default]);
 	}
 
 	public static function write($name, $data) {
-		Environment::set(true, ['features' => [$name => $data]]);
+		static::$_data = Set::merge(
+			static::$_data,
+			[$name => $data]
+		);
 	}
 
 	public static function enabled($name) {
-		return Environment::get('features.' . $name);
+		return (boolean) static::read($name);
 	}
 
 	public static function read($name = null) {
-		return Environment::get($name ? 'features.' . $name : 'features');
+		if (!$name) {
+			return static::$_data;
+		}
+		return static::_processDotPath($name, static::$_data);
+	}
+
+	protected static function _processDotPath($path, &$arrayPointer) {
+		if (isset($arrayPointer[$path])) {
+			return $arrayPointer[$path];
+		}
+		if (strpos($path, '.') === false) {
+			return null;
+		}
+		$pathKeys = explode('.', $path);
+		foreach ($pathKeys as $pathKey) {
+			if (!is_array($arrayPointer) || !isset($arrayPointer[$pathKey])) {
+				return false;
+			}
+			$arrayPointer = &$arrayPointer[$pathKey];
+		}
+		return $arrayPointer;
 	}
 }
 
--- extensions/cms/Settings.php
+++ extensions/cms/Settings.php
@@ -17,19 +17,54 @@ use lithium\util\Set;
 
 class Settings extends \lithium\core\StaticObject {
 
+	protected static $_data = [];
+
 	protected static $_sources = [];
 
 	public static function register($source, $name, $default = null) {
 		static::$_sources[$name] = $source;
-		Environment::set(true, Set::expand(['settings.' . $name => $default]));
+		static::$_data = Set::merge(static::$_data, Set::expand([$name => $default]));
 	}
 
 	public static function write($name, $data) {
-		Environment::set(true, Set::expand(['settings.' . $name => $data]));
+		$current = static::read($name);
+
+		if (is_array(($current)) && is_numeric(key($current))) {
+			// Prevent merging list like settings.
+			static::$_data = array_merge(
+				static::$_data,
+				Set::expand([$name => $data])
+			);
+		} else {
+			static::$_data = Set::merge(
+				static::$_data,
+				Set::expand([$name => $data])
+			);
+		}
 	}
 
 	public static function read($name = null) {
-		return Environment::get($name ? 'settings.' . $name : 'settings');
+		if (!$name) {
+			return static::$_data;
+		}
+		return static::_processDotPath($name, static::$_data);
+	}
+
+	protected static function _processDotPath($path, &$arrayPointer) {
+		if (isset($arrayPointer[$path])) {
+			return $arrayPointer[$path];
+		}
+		if (strpos($path, '.') === false) {
+			return null;
+		}
+		$pathKeys = explode('.', $path);
+		foreach ($pathKeys as $pathKey) {
+			if (!is_array($arrayPointer) || !isset($arrayPointer[$pathKey])) {
+				return false;
+			}
+			$arrayPointer = &$arrayPointer[$pathKey];
+		}
+		return $arrayPointer;
 	}
 }
 

b382c789c3af8cd7d377c4761a75012c8168a6f6
David M. Persson <davidpersson@gmx.de>
2014-03-26 17:53:05 +0100

    Adding missing dep.

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -19,6 +19,7 @@ use li3_flash_message\extensions\storage\FlashMessage;
 use lithium\g11n\Message;
 use lithium\security\Auth;
 use cms_core\extensions\cms\Features;
+use li3_mailer\action\Mailer;
 
 class UsersController extends \cms_core\controllers\BaseController {
 

257c9b244dcfc66e053b39858155ed31dd65dbdf
David Persson <davidpersson@gmx.de>
2014-04-24 15:58:05 +0200

    Adding users widget.

--- config/widgets.php
+++ config/widgets.php
@@ -12,6 +12,8 @@
 
 use lithium\g11n\Message;
 use cms_core\extensions\cms\Widgets;
+use cms_core\models\Users;
+use cms_core\models\VirtualUsers;
 
 extract(Message::aliases());
 
@@ -28,5 +30,30 @@ Widgets::register('cms_core', 'support', function() use ($t) {
 	'type' => Widgets::TYPE_QUICKDIAL,
 	'group' => Widgets::GROUP_DASHBOARD,
 ]);
+
+Widgets::register('cms_core', 'users', function() use ($t) {
+	$total = Users::find('count') + VirtualUsers::find('count');
+	$deactivated = Users::find('count', [
+		'conditions' => [
+			'is_active' => false
+		]
+	]);
+
+	return [
+		'class' => null,
+		'title' => $t('Users'),
+		'url' => [
+			'controller' => 'Users', 'library' => 'cms_core', 'admin' => true, 'action' => 'index'
+		],
+		'data' => [
+			$t('Total') => $total,
+			$t('Not active') => $deactivated
+		]
+	];
+}, [
+	'type' => Widgets::TYPE_COUNT_MULTIPLE_ALPHA,
+	'group' => Widgets::GROUP_DASHBOARD,
+]);
+
 
 ?>

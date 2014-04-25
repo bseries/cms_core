c8667258e5ed3b18072b33544ca32820896ae644
David Persson <davidpersson@gmx.de>
2014-04-25 17:04:55 +0200

    Fix typo in timestamp b.

--- extensions/data/behavior/Timestamp.php
+++ extensions/data/behavior/Timestamp.php
@@ -23,7 +23,7 @@ class Timestamp extends \li3_behaviors\data\model\Behavior {
 	protected static function _filters($model, $behavior) {
 		$model::applyFilter('save', function($self, $params, $chain) use ($behavior) {
 			if (isset($params['options']['whitelist'])) {
-				foreach ($behavior->config('field') as $field) {
+				foreach ($behavior->config('fields') as $field) {
 					$params['options']['whitelist'][] = $field;
 				}
 			}

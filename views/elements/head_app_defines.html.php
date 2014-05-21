64f523160cb192d74c7828bfe610f1cdb81225f6
David Persson <davidpersson@gmx.de>
2014-05-21 11:40:18 +0200

    Fix js

--- views/elements/head_app_defines.html.php
+++ views/elements/head_app_defines.html.php
@@ -7,12 +7,12 @@
 	App.media = {
 		base: '<?= $this->media->base() ?>',
 		endpoints: {
-			<?php if ($admin); ?>
+			<?php if ($admin): ?>
 				<?php $url = ['controller' => 'files', 'library' => 'cms_media', 'admin' => true] ?>
 				index: '<?= $this->url($url + ['action' => 'api_index']) ?>',
 				view: '<?= $this->url($url + ['action' => 'api_view', 'id' => '__ID__']) ?>',
 				transfer: '<?= $this->url($url + ['action' => 'api_transfer']) ?>'
-			?>
+			<?php endif ?>
 		}
 	};
 </script>

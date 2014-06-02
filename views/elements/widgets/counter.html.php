0cf070907851cf86cea16ba03132218d1c534fdd
David Persson <davidpersson@gmx.de>
2014-06-02 22:30:56 +0200

    Single counter widget.

--- views/elements/widgets/counter.html.php
+++ views/elements/widgets/counter.html.php
@@ -1,10 +1,16 @@
+<?php
+
+if (count($item['data']) <= 1) {
+	$item['class'] .= ' widget-counter-single';
+}
+
+?>
 <article class="widget widget-counter <?= $item['class'] ?>">
 	<?php if ($item['url']): ?>
 		<a href="<?= $this->url($item['url']) ?>">
 	<?php endif ?>
 		<h1 class="title"><?= $item['title'] ?></h1>
 
-		<?php if (count($item['data']) > 1): ?>
 			<div class="count-groups">
 			<?php foreach ($item['data'] as $title => $count): ?>
 				<div class="count-group">
@@ -19,18 +25,6 @@
 				</div>
 			<?php endforeach ?>
 			</div>
-		<?php else: ?>
-			<div class="no-count-groups">
-				<div class="title"><?= $title ?></div>
-				<div class="count">
-					<?php if (is_object($item['data'])): ?>
-						<?= $this->money->format($item['data'], 'money') ?>
-					<?php else: ?>
-						<?= $item['data'] ?>
-					<?php endif ?>
-				</div>
-			</div>
-		<?php endif ?>
 	<?php if ($item['url']): ?>
 		</a>
 	<?php endif ?>

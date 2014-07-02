b5cfd38846e6d6ce39c6ac590bf8a4a198415595
David Persson <davidpersson@gmx.de>
2014-07-02 16:10:26 +0200

    Debug widget rendering time.

--- views/pages/admin_home.html.php
+++ views/pages/admin_home.html.php
@@ -1,5 +1,7 @@
 <?php
 
+use lithium\analysis\Logger;
+
 $this->set([
 	'page' => [
 		'type' => 'standalone',
@@ -10,12 +12,21 @@ $this->set([
 ?>
 <article class="view-<?= $this->_config['controller'] . '-' . str_replace('_', '-', $this->_config['template']) ?>">
 	<div class="widgets">
-		<?php foreach ($widgets as $item): ?>
+		<?php foreach ($widgets as $name => $item): ?>
+			<?php
+				$start = microtime(true);
+			?>
 			<?=$this->view()->render(
 				['element' => 'widget_' . $item['type']],
 				['item' => $item['data']()],
 				['library' => 'cms_core']
 			) ?>
+			<?php
+				$took = microtime(true) - $start;
+				if ($took > 1) {
+					Logger::write('debug', "Widget {$name} took " . round($took, 4) . "s to render.");
+				}
+			?>
 		<?php endforeach ?>
 	</div>
 </article>

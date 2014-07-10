453be01e378b58a8043ddbc2b7df7d4a5fe53eea
David Persson <davidpersson@gmx.de>
2014-07-10 11:59:03 +0200

    Enable help for selects.

--- assets/js/views/layouts/admin.js
+++ assets/js/views/layouts/admin.js
@@ -45,7 +45,7 @@ require(['jquery', 'list', 'nprogress', 'notify', 'domready!'], function($, List
   //
   $('form .help').each(function() {
     var $help = $(this);
-    var $input = $help.prev().find('input,textarea');
+    var $input = $help.prev().find('input,textarea,select');
 
     $input.on('focusin', function() {
       $help.fadeIn();

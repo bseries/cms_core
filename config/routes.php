0b8d60374e21f692d1376f505ebd526e35e05530
David Persson <davidpersson@gmx.de>
2014-07-26 17:49:55 +0200

    Updating routes.

--- config/routes.php
+++ config/routes.php
@@ -50,22 +50,7 @@ Router::connect('/admin/logout', [
 	'controller' => 'users', 'action' => 'logout', 'library' => 'cms_core', 'admin' => true
 ], $persist);
 
-Router::connect('/admin/support', [
-	'controller' => 'pages', 'action' => 'support', 'library' => 'cms_core', 'admin' => true
-], $persist);
-
-// Do not persist admin as we render this in the apps default layout context.
-Router::connect('/admin/styleguide', [
-	'controller' => 'pages', 'action' => 'styleguide', 'library' => 'cms_core', 'admin' => true
-], ['controller']);
-
-Router::connect('/admin/tokens/{:action}/{:token:[0-9a-f]{8,16}}', [
-	'controller' => 'tokens', 'library' => 'cms_core', 'admin' => true
-], $persist);
-Router::connect('/admin/tokens/{:action}/{:args}', [
-	'controller' => 'tokens', 'library' => 'cms_core', 'admin' => true
-], $persist);
-
+// Users
 Router::connect('/admin/users/{:id:[0-9]+}/change-role/{:role}', [
 	'controller' => 'users', 'action' => 'change_role', 'library' => 'cms_core', 'admin' => true
 ], $persist);
@@ -84,8 +69,14 @@ Router::connect('/admin/virtual-users/{:action}/{:id:[0-9]+}', [
 Router::connect('/admin/virtual-users/{:action}/{:args}', [
 	'controller' => 'VirtualUsers', 'library' => 'cms_core', 'admin' => true
 ], $persist);
+Router::connect('/admin/tokens/{:action}/{:token:[0-9a-f]{8,16}}', [
+	'controller' => 'tokens', 'library' => 'cms_core', 'admin' => true
+], $persist);
+Router::connect('/admin/tokens/{:action}/{:args}', [
+	'controller' => 'tokens', 'library' => 'cms_core', 'admin' => true
+], $persist);
 
-
+// Addresses
 Router::connect('/admin/addresses/{:action}/{:id:[0-9]+}', [
 	'controller' => 'addresses', 'library' => 'cms_core', 'admin' => true
 ], $persist);
@@ -93,8 +84,23 @@ Router::connect('/admin/addresses/{:action}/{:args}', [
 	'controller' => 'addresses', 'library' => 'cms_core', 'admin' => true
 ], $persist);
 
+// Settings, Misc
 Router::connect('/admin/settings', [
 	'controller' => 'settings', 'action' => 'index', 'library' => 'cms_core', 'admin' => true
 ], $persist);
+Router::connect('/admin/support', [
+	'controller' => 'pages', 'action' => 'support', 'library' => 'cms_core', 'admin' => true
+], $persist);
+
+// Do not persist admin as we render this in the apps default layout context.
+Router::connect('/admin/styleguide', [
+	'controller' => 'pages', 'action' => 'styleguide', 'library' => 'cms_core', 'admin' => true
+], ['controller']);
+
+// Administration JavaScript Environment
+Router::connect('/admin/api/discover', [
+	'controller' => 'app', 'action' => 'api_discover', 'library' => 'cms_core', 'admin' => true
+], $persist);
+
 
 ?>

87c73c9429e154847455c8d5fc4aa84655d95874
David Persson <davidpersson@gmx.de>
2014-08-08 13:51:45 +0200

    Drop first param from settings/features.

--- config/settings.php
+++ config/settings.php
@@ -13,52 +13,52 @@
 use cms_core\extensions\cms\Settings;
 use cms_core\extensions\cms\Features;
 
-Settings::register('cms_core', 'project.name', PROJECT_NAME);
-Settings::register('cms_core', 'project.version', PROJECT_VERSION);
-Settings::register('cms_core', 'site.title');
+Settings::register('project.name', PROJECT_NAME);
+Settings::register('project.version', PROJECT_VERSION);
+Settings::register('site.title');
 
 // FIXME Use a pseudo number generator seeded with project
 // name to generate cookie secret. Simple md5'ing wont work as
 // there the alphabet would be too limited for a password style string.
-Settings::register('cms_core', 'security.cookieSecret', 'alsFDDT§$sdfs');
+Settings::register('security.cookieSecret', 'alsFDDT§$sdfs');
 
-Settings::register('cms_core', 'contact.default.name', 'Acme Inc.');
-Settings::register('cms_core', 'contact.default.type', 'organization'); // organization or person
-Settings::register('cms_core', 'contact.default.email', 'mail@example.com');
-Settings::register('cms_core', 'contact.default.phone', '+49 (0) 12 345 678');
-Settings::register('cms_core', 'contact.default.city', 'Las Vegas');
+Settings::register('contact.default.name', 'Acme Inc.');
+Settings::register('contact.default.type', 'organization'); // organization or person
+Settings::register('contact.default.email', 'mail@example.com');
+Settings::register('contact.default.phone', '+49 (0) 12 345 678');
+Settings::register('contact.default.city', 'Las Vegas');
 
-Settings::register('cms_core', 'contact.exec.name', 'Atelier Disko');
-Settings::register('cms_core', 'contact.exec.type', 'organization');
-Settings::register('cms_core', 'contact.exec.email', 'info@atelierdisko.de');
-Settings::register('cms_core', 'contact.exec.phone', '+49 (0) 40 355 618 96');
-Settings::register('cms_core', 'contact.exec.website', 'http://atelierdisko.de');
-Settings::register('cms_core', 'contact.exec.postal_code', 'D-20359');
-Settings::register('cms_core', 'contact.exec.street_address', 'Budapester Straße 49');
-Settings::register('cms_core', 'contact.exec.city', 'Hamburg');
-Settings::register('cms_core', 'contact.exec.country', 'Germany');
-Settings::register('cms_core', 'contact.exec.district', 'St. Pauli');
+Settings::register('contact.exec.name', 'Atelier Disko');
+Settings::register('contact.exec.type', 'organization');
+Settings::register('contact.exec.email', 'info@atelierdisko.de');
+Settings::register('contact.exec.phone', '+49 (0) 40 355 618 96');
+Settings::register('contact.exec.website', 'http://atelierdisko.de');
+Settings::register('contact.exec.postal_code', 'D-20359');
+Settings::register('contact.exec.street_address', 'Budapester Straße 49');
+Settings::register('contact.exec.city', 'Hamburg');
+Settings::register('contact.exec.country', 'Germany');
+Settings::register('contact.exec.district', 'St. Pauli');
 
-Settings::register('cms_core', 'service.googleAnalytics.default.account');
-Settings::register('cms_core', 'service.googleAnalytics.default.domain');
-Settings::register('cms_core', 'service.googleAnalytics.default.propertyId');
+Settings::register('service.googleAnalytics.default.account');
+Settings::register('service.googleAnalytics.default.domain');
+Settings::register('service.googleAnalytics.default.propertyId');
 
-Settings::register('cms_core', 'user.number', [
+Settings::register('user.number', [
 	'sort' => '/([0-9]{4}-[0-9]{4})/',
 	'extract' => '/[0-9]{4}-([0-9]{4})/',
 	'generate' => '%Y-%%04.d'
 ]);
 
-Settings::register('cms_core', 'availableCountries', [
+Settings::register('availableCountries', [
 	'DE', 'US', 'CA'
 ]);
 
-Settings::register('cms_core', 'availableCurrencies', [
+Settings::register('availableCurrencies', [
 	'EUR', 'USD'
 ]);
 
-Features::register('cms_core', 'useNewGoogleAnalyticsTrackingCode', true);
-Features::register('cms_core', 'useBilling', false);
-Features::register('cms_core', 'user.sendActivationMail', false);
+Features::register('useNewGoogleAnalyticsTrackingCode', true);
+Features::register('useBilling', false);
+Features::register('user.sendActivationMail', false);
 
 ?>

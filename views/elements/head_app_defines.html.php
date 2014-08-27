357233c0c4e3ddb22585f5fe7121fe24f1120502
David Persson <davidpersson@gmx.de>
2014-08-27 16:53:32 +0200

    Allow app discover url.

--- views/elements/head_app_defines.html.php
+++ views/elements/head_app_defines.html.php
@@ -7,7 +7,11 @@ App = {
 		base: "<?= $this->media->base() ?>"
 	},
 	api: {
-		discover: "<?= $this->url(['library' => 'cms_core', 'action' => 'api_discover', 'admin' => true, 'controller' => 'App']) ?>"
+		<?php if ($admin): ?>
+			discover: "<?= $this->url(['library' => 'cms_core', 'action' => 'api_discover', 'admin' => true, 'controller' => 'App']) ?>"
+		<?php else: ?>
+			discover: "<?= $this->url(['action' => 'api_discover', 'controller' => 'App']) ?>"
+		<?php endif ?>
 	}
 }
 </script>

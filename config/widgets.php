34493c82e8736fc115b4112820ce512d9725a001
David Persson <davidpersson@gmx.de>
2015-07-24 15:37:17 +0200

    Add widget skel.

---
+++ config/widgets.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * CMS Core
+ *
+ * Copyright (c) 2014 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+namespace cms_core\config;
+
+use base_core\extensions\cms\Widgets;
+
+Widgets::register('authoring',  function() {
+	return [
+		'data' => []
+	];
+}, [
+	'type' => Widgets::TYPE_TABLE,
+	'group' => Widgets::GROUP_DASHBOARD,
+	'weight' => Widgets::WEIGHT_HIGH
+]);
+
+?>

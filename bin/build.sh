c8e47c98dbae6dfe72ac4f2472b32912b1270c77
Marius Wilms <mariuswilms@gmx.de>
2018-02-14 11:46:51 +0100

    Use babel env preset.

--- bin/build.sh
+++ bin/build.sh
@@ -17,13 +17,22 @@ for f in $(ls resources/g11n/po/*/LC_MESSAGES/*.po); do
 	msgfmt -o ${f/.po/.mo} --verbose $f
 done
 
-# Babelify in-place for ES2015 compatiblity. Once we do not want to support IE11
-# and iOS Safari <= 9.3 anymore we can safely remove this line or use babel
-# to continously upgrade supported ECMAScript versions.
-babel assets/js \
-	-d assets/js \
-	--presets babel-preset-es2015 \
-	--ignore underscore.js,require.js,require,jquery.js,modernizr.js,wysihtml5.js
+# Babelify in-place for full current ESx compatiblity.
+cat << EOF > .babelrc
+{
+	"presets": [
+		["env", {"targets": {"browsers": [
+			"last 2 versions",
+			"> 5%",
+			"ie 11"
+		]}}]
+	],
+	"ignore": [
+		"wysihtml5.js"
+	]
+}
+EOF
+babel assets/js -d assets/js
 
 for f in $(find assets/js -type f -name *.js); do
 	uglifyjs --compress --mangle -o $f.min -- $f && mv $f.min $f
